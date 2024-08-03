e55a6258d3c8b930d9a3aacca4e2defa304d300a
Robert21AV <138814776+Robert21AV05@users.noreply.github.com>
2024-08-03 17:32:08 -0500

    Changing aosp_dipper and adding PixelOS Flags

--- aosp_dipper.mk
+++ aosp_dipper.mk
@@ -7,10 +7,10 @@
 $(call inherit-product, device/xiaomi/dipper/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_dipper
+PRODUCT_NAME := aosp_dipper
 PRODUCT_DEVICE := dipper
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
@@ -25,3 +25,14 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT="dipper"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Flags
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
